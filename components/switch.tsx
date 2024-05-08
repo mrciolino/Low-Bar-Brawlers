03fa7f30e70b1b4b01ef00a4aa3debd32b8983a8
mrciolino <mrciolino@comcast.net>
2024-05-08 15:23:48 -0400

    allow images and make default dark mode

--- components/switch.tsx
+++ components/switch.tsx
@@ -24,7 +24,7 @@ export function ModeToggle() {
 
     return (
         <Button variant="outline" size="icon" onClick={toggleTheme}>
-            {theme === "light" ? (
+            {theme === "dark" ? (
                 <>
                     <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
                     <span className="sr-only">Switch to light mode</span>
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     // Uncomment the following line to build a static site.
     output: "export",
     reactStrictMode: true,
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
