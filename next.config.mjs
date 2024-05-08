6fc70cd8a79b88a477ea4f64a1fd95d3b64dc659
mrciolino <mrciolino@comcast.net>
2024-05-08 15:13:17 -0400

    render stuff for static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Uncomment the following line to build a static site.
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
